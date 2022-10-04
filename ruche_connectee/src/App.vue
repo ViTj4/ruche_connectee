16a367f37d92c582c746ce08cf423097cb77bdf3
louis <l.hanquiez22@gmail.com>
2022-10-04 11:54:53 +0200

    ++ importation component

--- ruche_connectee/src/App.vue
+++ ruche_connectee/src/App.vue
@@ -2,12 +2,22 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import NavbarVue from './components/Navbar.vue';
+import CapteurComponent from './components/CapteurComponent.vue';
+import CapteurGPSComponent from './components/CapteurGPSComponent.vue';
+import MeteoComponent from './components/MeteoComponent.vue';
+import OuvertureComponent from './components/OuvertureComponent.vue'
+import TemperatureComponent from './components/TemperatureComponent.vue'
 </script>
 
 <template>
 
   <NavbarVue></NavbarVue>
-
+  <TemperatureComponent></TemperatureComponent>
+  <CapteurComposent></CapteurComposent>
+  <CapteurGPSComponent></CapteurGPSComponent>
+  <MeteoComponent></MeteoComponent>
+  <OuvertureComponent></OuvertureComponent>
+  
 
 
 </template>
